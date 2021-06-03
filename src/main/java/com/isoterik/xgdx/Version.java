efb0d4faeaae2912ca36aada91e1251bc09abc8b
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-06-03 19:41:55 +0100

    added libgdx v1.10.0

--- src/main/java/com/isoterik/xgdx/Version.java
+++ src/main/java/com/isoterik/xgdx/Version.java
@@ -1,4 +1,11 @@
 package com.isoterik.xgdx;
 
 public class Version {
+    public static final int MAJOR      = 1;
+
+    public static final int MINOR      = 0;
+
+    public static final int REVISION   = 0;
+
+    public static final String CURRENT = MAJOR + "." + MINOR + "." + REVISION;
 }
